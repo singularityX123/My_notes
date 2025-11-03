04cd0b1a0a16898f605e891bb22551f0ebf11d74
wenzhechai <1534297980@qq.com>
2025-11-03 19:22:40 +0800

    2025.11.03

--- main.c
+++ main.c
@@ -1,34 +1,17 @@
 ﻿#include <stdio.h>
-//简单选择排序
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
-        int min_idx = i;
-        for (int j = i+1; j < n; j++) {
-            if (arr[j] < arr[min_idx]) {
-                min_idx = j;
-            }
-        }
-        // 交换
-        int temp = arr[i];
-        arr[i] = arr[min_idx];
-        arr[min_idx] = temp;
-    }
+
+void func(int **p) {
+    int x = 100;
+    *p = &x;
 }
 
-
-int main()
-{
-    int num[11];
-    for (int i = 0 ; i < 10; i++){
-        scanf("%d", &num[i]);
-        getchar();
+int main() {
+    char str1[] = "Hello";
+    char str2[] = "Hello";
+    if (str1 == str2) {
+        printf("Equal\n");
+    } else {
+        printf("Not Equal\n");
     }
-
-    selectionSort(num, 10);
- 
-    for (int i = 0 ; i < 10; i++){
-        printf("%d ", num[i]);
-    }
-
     return 0;
 }
