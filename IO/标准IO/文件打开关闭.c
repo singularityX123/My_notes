a70a4855515d2df8490067d25972d0f8e38f634f
remote_wz <1534297980@qq.com>
2025-11-27 00:10:22 +0800

    2025.11.26<c>

---
+++ IO/标准IO/文件打开关闭.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+int main()
+{
+    FILE *fp = NULL; // 定义文件指针，初始化为空指针
+    fp = fopen("buffered.txt", "r"); // 只读打开文件
+    if (fp == NULL)
+    {
+        printf("文件打开失败");
+        return -1;
+    }
+
+    printf("文件打开成功\n");
+    fclose(fp); // 关闭文件，释放资源防止被其他程序占用（安全漏洞）
+    fp = NULL; // 避免野指针
+    return 0;
+}
---
+++ IO/标准IO/文件错误处理信息.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+int main(int argc, char * argv[]) {
+ if(argc < 2){
+  printf("Usage: %s <filename>\n", argv[0]);
+  return -1;
+ }
+ const char * filename = argv[1];
+ // 尝试打开文件
+ FILE * fp = fopen(filename, "r");
+ if (fp == NULL) {
+  // 使用 perror 输出错误信息
+  perror(filename);
+  // 获取 errno 对应的错误描述字符串
+  const char *errmsg = strerror(errno);
+  // 手动格式化并输出错误信息
+  fprintf(stderr, "%s %s\n", filename, errmsg);
+ } else {
+  fprintf(stdout,"%s Open success\n", filename);
+  // 文件打开成功，关闭文件
+  fclose(fp);
+ }
+ return 0;
+}
