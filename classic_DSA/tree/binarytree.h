97826b208d4e826c6429f8661182b538d8788534
remote_wz <1534297980@qq.com>
2025-11-27 17:36:57 +0800

    1

---
+++ classic_DSA/tree/binarytree.h
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef char data_t;
+
+typedef struct node_t {
+    data_t data;
+    struct node_t *lchild;
+    struct node_t *rchild;
+} bit_tree;
+
+bit_tree* create_bit_tree();
+
