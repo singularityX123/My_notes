9944a211b8e1085b545015c60791ca46d47b1d0d
singularityX123 <1534297980@qq.com>
2026-07-08 15:02:08 +0800

    1

---
+++ 17.BSP/嵌入式linux内核及驱动开发/HelloDriver.c
@@ -0,0 +1,17 @@
+﻿#include <linux/init.h>
+#include <linux/module.h>
+
+static int __init hellodriver_init(void) // GNU C
+{
+    printk("Hello, driver!\n");
+    return 0;
+}
+
+static void __exit hellodriver_exit(void)
+{
+    printk("Goodbye, driver!\n");
+}
+
+module_init(hellodriver_init);
+module_exit(hellodriver_exit);
+MODULE_LICENSE("GPL");
